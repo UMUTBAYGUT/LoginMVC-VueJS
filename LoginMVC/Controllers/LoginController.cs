a4fde020379c2b9ecb248af395b98383ffde2069
Umut BAYĞUT <UMUTBAYGUT@users.noreply.github.com>
2019-02-21 18:19:57 +0300

    Add project files.

---
+++ LoginMVC/Controllers/LoginController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public UserClass Index(string username, string password)
+        {
+            if (username == "umut")
+            {
+                if (password == "gulsum")
+                {
+                    var user2 = new UserClass()
+                    {
+                        IsLogin = true,
+                        Name = "Umut",
+                        Role = "admin"
+                    };
+                    return user2;
+                }
+            }
+
+            var user = new UserClass()
+            {
+                IsLogin = false
+            };
+            return user;
+        }
+
+        [EnableCors]
+
+        [HttpPost]
+        public UserClass Login([FromBody]requestClass Arguments)
+        {
+            if (Arguments.username == "umut")
+            {
+                if (Arguments.password == "gulsum")
+                {
+                    var user2 = new UserClass()
+                    {
+                        IsLogin = true,
+                        Name = "Umut",
+                        Role = "admin"
+                    };
+                    return user2;
+                }
+            }
+
+            var user = new UserClass()
+            {
+                IsLogin = false
+            };
+            return user;
+        }
+    }
+
+    public class UserClass
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+        public bool IsLogin { get; set; }
+    }
+
+    public class requestClass
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
